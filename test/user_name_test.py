9189ab1282d1ded535f3a1f14e786a5ca84bd6e8
Kel Kanhirun <kkanhirun@gmail.com>
2015-04-10 21:42:19 -0500

    test: adds remaining tests which all passes

--- test/user_name_test.py
+++ test/user_name_test.py
@@ -36,21 +36,59 @@ class TestUserName(TestCase):
     self.assertEqual("TygerTyger2", suggestedName)
 
 
-#  def testTygerTygerWithLargeVariantButNoBase(self):
-#    userName      = UserName()
-#    existingNames = set(["TygerTyger2000",
-#                         "TygerTyger1",
-#                         "MasterDisaster",
-#                         "DingBat",
-#                         "Orpheus",
-#                         "WolfMan",
-#                         "MrKnowItAll"])
-#    newName       = "TygerTyger"
-#
-#    suggestedName = userName.newMember(newName)
-#
-#    self.assertEqual("TygerTyger", suggestedName)
-#
-#
-#  def testCaseSensitiveUserNames(self):
-#
+  def testTygerTygerWithLargeVariantButNoBase(self):
+    existingNames = set(["TygerTyger2000",
+                         "TygerTyger1",
+                         "MasterDisaster",
+                         "DingBat",
+                         "Orpheus",
+                         "WolfMan",
+                         "MrKnowItAll"])
+    newName       = "TygerTyger"
+    userName      = UserName(existingNames)
+
+    suggestedName = userName.newMember(newName)
+
+    self.assertEqual("TygerTyger", suggestedName)
+
+
+  def testCaseSensitiveUserNames(self):
+    existingNames = set(["grokster2",
+                         "BrownEyedBoy",
+                         "Yoop",
+                         "BlueEyedGirl",
+                         "grokster",
+                         "Elemental",
+                         "NightShade",
+                         "Grokster1"])
+    newName       = "grokster"
+    userName      = UserName(existingNames)
+
+    suggestedName = userName.newMember(newName)
+
+    self.assertEqual("grokster1", suggestedName)
+
+
+  def testBartWithLargeVariant(self):
+    existingNames = set(["Bart4",
+                         "Bart5",
+                         "Bart6",
+                         "Bart7",
+                         "Bart8",
+                         "Bart9",
+                         "Bart10",
+                         "Lisa",
+                         "Marge",
+                         "Homer",
+                         "Bart",
+                         "Bart1",
+                         "Bart2",
+                         "Bart3",
+                         "Bart11",
+                         "Bart12"])
+    newName      = "Bart"
+    userName     = UserName(existingNames)
+
+    suggestedName = userName.newMember(newName)
+
+    self.assertEqual("Bart13", suggestedName)
